7c7c4ea7c2f6414142ed7797bc8755c4b0cbbed1
ngtrbaolong <ngtrbl@gmail.com>
2025-12-03 16:16:25 +0700

    create ui signup signup from

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,24 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Toaster } from "sonner";
-import { BrowserRouter, Routes, Route } from "react-router";
+
 import HomePage from "./pages/HomePage";
+import SignInPage from "./pages/SignInPage";
+import SignUpPage from "./pages/SignUpPage";
 import NotFound from "./pages/NotFound";
 
-function App() {
-  return <>
-
-    <Toaster richColors />
-
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
-
-
-  </>
+export default function App() {
+  return (
+    <>
+      <Toaster richColors />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/signin" element={<SignInPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
-
-export default App
